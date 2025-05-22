f326513494d2c21acfa618f8afc5f813d89670f3
Failed-Captcha <crowe.elshobary@gmail.com>
2025-05-21 21:16:32 -0400

    updated script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("text%20files/cpu.txt")
+    fetch("text/cpu.txt")
         .then(response => response.text())
         .then(data => {
             document.getElementById('text-content').textContent = data;
