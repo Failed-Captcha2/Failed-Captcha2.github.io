1aaaf462815fb7771ce685503682f36fd37f87ec
Failed-Captcha <crowe.elshobary@gmail.com>
2026-01-04 11:43:21 -0500

    created V2 of website

---
+++ old/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("./text/cpu.txt")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('text-content').textContent = data;
+        })
+        .catch(error => console.error("Fetch error:", error));
+});
--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("./text/cpu.txt")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('text-content').textContent = data;
-        })
-        .catch(error => console.error("Fetch error:", error));
-});
+// links 
+
+const github = document.getElementById("github");
+showwave(github);
+github.addEventListener("mouseenter", () => showlink(github, "https://github.com/Failed-Captcha2"));
+github.addEventListener("mouseleave", () => showwave(github));
+
+const linkedin = document.getElementById("linkedin");
+showwave(linkedin);
+linkedin.addEventListener("mouseenter", () => showlink(linkedin, "https://www.linkedin.com/in/h-corey-elshobary-718864360/"));
+linkedin.addEventListener("mouseleave", () => showwave(linkedin));
+
+const email = document.getElementById("email");
+showwave(email);
+email.addEventListener("mouseenter", () => showlink(email, "mailto:crowe.elshobary@gmail.com"));
+email.addEventListener("mouseleave", () => showwave(email));
+
+function showlink(link, url){
+    link.innerHTML = "<a href='" + url + "'>" + url + "</a>";
+}
+
+function showwave(link) {
+    link.innerHTML = "<p>~~~~~~~~~~~~~~~~~~~~~</p>";
+}
