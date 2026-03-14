743c90222923a6f58edef9cae25272a7914af5d5
Failed-Captcha <crowe.elshobary@gmail.com>
2026-03-14 17:55:05 -0400

    added buttons

--- old/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("./text/cpu.txt")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('text-content').textContent = data;
-        })
-        .catch(error => console.error("Fetch error:", error));
-});
